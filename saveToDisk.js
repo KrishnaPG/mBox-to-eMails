eaeaa445bbc4d9bb1097fbc3dd753f9ff1b8bae2
Gopalakrishna Palem <KrishnaPG@users.noreply.github.com>
2024-12-08 06:04:11 +0530

    Cleanup code

--- saveToDisk.js
+++ saveToDisk.js
@@ -1,7 +1,4 @@
 export default function (parsedMail, outFolder, id) {
-  //const fileName = `R:/out-${parsedMail.date.toISOString().replaceAll(':', '-')}.json`;
     const fileName = `${outFolder}/${id}-${Math.ceil(Math.random() * 100000)}.eml`;
-  //console.log('--------wrote: ', fileName);
   return Bun.write(fileName, parsedMail);
-   //return Bun.write(fileName, JSON.stringify(parsedMail, null, 2));
 }
